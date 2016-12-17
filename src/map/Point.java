8634760dda9200edf0d9f966f694cd05debca184
Boldorf Smokebane <thenamelessdwarf@gmail.com>
2016-12-16 18:50:54 -0600

    Update Point with changes from EverSector

--- src/map/Point.java
+++ src/map/Point.java
@@ -3,12 +3,30 @@ package map;
 /** A point on a map, represented with two coordinates: x and y. */
 public class Point
 {
-    int x, y;
+    /** The x coordinate of the point. */
+    int x;
     
-    public Point() {this(0, 0);}
+    /** The y coordinate of the point. */
+    int y;
     
-    public Point(Point p) {this(p.x, p.y);}
+    /** Creates a new point at (0, 0). */
+    public Point()
+        {this(0, 0);}
     
+    /**
+     * Copying constructor that creates a point at the same coordinates as
+     * another point.
+     * @param p the point whose coordinates will be used in the construction of
+     * this point
+     */
+    public Point(Point p)
+        {this(p.x, p.y);}
+    
+    /**
+     * Creates a point with the given x and y coordinates.
+     * @param xx the x coordinate of the point
+     * @param yy the y coordinate of the point
+     */
     public Point(int xx, int yy)
     {
         x = xx;
@@ -17,17 +35,17 @@ public class Point
     
     /**
      * Creates a point from an ordered pair in String form.
-     * @param op the ordered pair as a String
+     * @param pair the ordered pair as a String
      */
-    public Point(String op)
+    public Point(String pair)
     {
-        op = op.replace("(", "");
-        op = op.replace(")", "");
-        op = op.replace(",", "");
-        String[] split = op.split(" ");
+        pair = pair.replace("(", "");
+        pair = pair.replace(")", "");
+        pair = pair.replace(",", "");
+        String[] split = pair.split(" ");
         
-        x = Integer.parseInt(split[0]);
-        y = Integer.parseInt(split[1]);
+        x = core.Prompt.parseInt(split[0], 0);
+        y = core.Prompt.parseInt(split[1], 0);
     }
     
     @Override
@@ -42,6 +60,11 @@ public class Point
     public void changeX(int xx) {x += xx;}
     public void changeY(int yy) {y += yy;}
     
+    /**
+     * Sets the coordinates to the given ones.
+     * @param xx the new x coordinate of the point
+     * @param yy the new y coordinate of the point
+     */
     public void set(int xx, int yy)
     {
         x = xx;
@@ -51,26 +74,40 @@ public class Point
     /**
      * Returns true if another entered point has the same coordinates as this
      * point.
-     * @param point the point to compare for equality
+     * @param point the point to compare for equality, must be non-null
      * @return true if the x coordinates and y coordinates are equal across
      * both points
      */
     public boolean equals(Point point)
-        {return point.x == x && point.y == y;}
+    {
+        if (point == null)
+            return false;
+        
+        return point.x == x && point.y == y;
+    }
     
     /**
      * Returns true if another specified point is exactly one space away from
      * this point.
-     * @param point the point to compare for adjacency
+     * @param point the point to compare for adjacency, must be non-null
      * @return true if a coordinate of the point deviates exactly one cardinal
      * space from the corresponding coordinate on this point, assuming the other
      * coordinate does not
      */
     public boolean isAdjacentTo(Point point)
+        {return orthagonalDistanceTo(point) == 1;}
+    
+    /**
+     * Returns the distance to another point, moving only on the x and y plane
+     * by one unit at a time.
+     * @param point the point to get the distance to, must be non-null
+     * @return 
+     */
+    public int orthagonalDistanceTo(Point point)
     {
-        return ((Math.abs(x - point.x) == 1)  && 
-                (Math.abs(y - point.y) == 0)) ||
-               ((Math.abs(x - point.x) == 0)  &&
-                (Math.abs(y - point.y) == 1));
+        if (point == null)
+            return -1;
+        
+        return Math.abs(x - point.x) + Math.abs(y - point.y);
     }
 }
