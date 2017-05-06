a4c4384ffafce1285f6134f44f7180703620074f
Boldorf Smokebane <thenamelessdwarf@gmail.com>
2017-05-06 09:34:47 -0500

    Remove specific fields from Being
    In an effort to make Being and AI more customizable, the speed and
    inventories fields of Being have been removed. If these need to be
    implemented in a project, then Being can be extended. In any case, AI is
    probably a better candidate for managing speed and actions, and it was
    designed to be extended initially.

--- src/corelib/map/Being.java
+++ src/corelib/map/Being.java
@@ -1,27 +1,15 @@
 package corelib.map;
 
 import corelib.display.glyphs.ColorChar;
-import corelib.items.Container;
 import squidpony.squidmath.Coord;
 
 /** A more fleshed out Entity capable of acting on its own. */
 public class Being extends Entity
 {
-    private double speed;
     private AI ai;
-    // Replace with a BodyPart[] eventually
-    private Container[] inventories;
     
-    public Being(TileMap map, Coord location, ColorChar glyph, double moveSpeed,
-            Container[] inventories)
-    {
-        super(map, location, glyph);
-        this.inventories = inventories;
-        this.speed = moveSpeed;
-    }
-    
-    public double getMoveSpeed()
-        {return speed;}
+    public Being(TileMap map, Coord location, ColorChar glyph)
+        {super(map, location, glyph);}
     
     public void setAI(AI ai)
         {this.ai = ai;}
