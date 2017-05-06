8852301dc50e2250896f42ae7a706dca34660008
Boldorf Smokebane <thenamelessdwarf@gmail.com>
2017-05-05 21:40:37 -0500

    Simplify the directory system
    Directories now have a type parameter for their content, rendering the
    Content interface unnecessary. Thus, Directory has been moved to corelib
    and documented.

--- src/corelib/directories/Content.java
+++
@@ -1,9 +0,0 @@
-package corelib.directories;
-
-/** An item in a directory. */
-public interface Content
-{
-    public Directory getDirectory();
-    
-    public void print();
-}
--- src/corelib/directories/Directory.java
+++
@@ -1,9 +0,0 @@
-package corelib.directories;
-
-/** An object that stores a list of other objects in it. */
-public interface Directory
-{
-    public String listContents();
-    public Content findContent(String name);
-    public Content deepSearch(String name);
-}
