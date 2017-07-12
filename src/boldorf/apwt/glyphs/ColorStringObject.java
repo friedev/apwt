ccc077d4220437fc9e29c80667b1468a1431839a
Boldorf Smokebane <thenamelessdwarf@gmail.com>
2017-07-12 12:31:07 -0500

    Add documentation to ColorStringObject.toColorString() and the boldorf.util package

--- src/boldorf/apwt/glyphs/ColorStringObject.java
+++ src/boldorf/apwt/glyphs/ColorStringObject.java
@@ -3,5 +3,9 @@ package boldorf.apwt.glyphs;
 /** An object with a {@link #toColorString()} method. */
 public interface ColorStringObject
 {
+    /**
+     * Returns a {@link ColorString} representing this object.
+     * @return a {@link ColorString} representing this object
+     */
     public ColorString toColorString();
 }
---
+++ src/boldorf/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * General utilities that can be used separately of APWT's display components,
+ * though certain library components depend on them.
+ */
+package boldorf.util;
